69834d37432a87b0d1529139c4f56cd1f16993b7
akenihan <46343185+akenihan@users.noreply.github.com>
2019-01-16 09:44:12 +0000

    Update commands.sh

--- 02-ManipulateDataUsingMongoShell/01-create_and_read_back_data/commands.sh
+++ 02-ManipulateDataUsingMongoShell/01-create_and_read_back_data/commands.sh
@@ -4,4 +4,4 @@ wget -q https://git.io/vFb1J - O /tmp/setupmongodb.sh && source /tmp/setupmongod
 # Connect to the Mongo Database
 # NOTE: REPLACE THE `<dbuser>` & `<dbpassword>` WITH YOUR OWN PERSONAL
 # VALUES (WITHOUT THE ANGULAR BRACKETS)
-mongo ds159812.mlab.com:59812/mytestdb -u <dbuser> -p <dbpassword>
+mongo ds157834.mlab.com:57834/mytestdb -u <dbuser> -p <dbpassword>
